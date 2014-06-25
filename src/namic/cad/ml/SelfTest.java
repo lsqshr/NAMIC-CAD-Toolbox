39cead673f938ffb668848eac5cd65f3550ccaba
sliu7418 <sliu7418@uni.sydney.edu.au>
2014-06-25 09:41:53 -0400

    Merge into Eclipse Sidong

---
+++ src/namic/cad/ml/SelfTest.java
@@ -0,0 +1,24 @@
+package namic.cad.ml;
+
+import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.mathutil.randomize.ConsistentRandomizer;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.back.Backpropagation;
+import java.io.IOException;
+
+
+public class SelfTest {
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
